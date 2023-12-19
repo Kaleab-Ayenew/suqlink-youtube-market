973c9f45e6e9189d25a9dd0909bd656a83302c21
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-12-19 14:37:06 +0300

    Added contact info

--- src/layouts/_copyright.tsx
+++ src/layouts/_copyright.tsx
@@ -6,15 +6,19 @@ export default function Copyright({ className }: { className?: string }) {
   const { t } = useTranslation('common');
   return (
     <div className={cn('tracking-[0.2px]', className)}>
-      &copy; {'Copyright'} {currentYear} {'by'}{' '}
-      <a
-        href="https://suqlink.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-brand-dark"
-      >
-        Suqlink
-      </a>
+      <ul style={{ textAlign: 'center', fontSize: '13px', padding: '9px' }}>
+        <li>
+          <strong>Contact us about anything:</strong>
+        </li>{' '}
+        <li style={{ color: '#8a8888', margin: '3px' }}>
+          Phone: +251953640216
+        </li>{' '}
+        <li style={{ color: '#8a8888', margin: '3px' }}>
+          Telegram: <a href="https://t.me/kal_suqlink">@kal_suqlink</a>
+        </li>{' '}
+        <li style={{ color: '#8a8888' }}>Email: info@suqlink.com</li>
+        {'\n\n'}
+      </ul>
     </div>
   );
 }
